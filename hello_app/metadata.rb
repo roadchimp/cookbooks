206a6f47f00f5d74b99631b08c697b343094bb1b
Roadchimp <roadchimp@gmail.com>
2013-01-25 19:08:15 +0000

    Add cookbook for hello world LAMP application.

---
+++ hello_app/metadata.rb
@@ -0,0 +1,9 @@
+name             'hello_app'
+maintainer       'RoadChimp'
+maintainer_email 'roadchimp@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures hello_app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          "apache2"
+depends          "mysql"
